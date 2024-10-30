096fbe9890175fa9e160f6d7ffc74fed677684f4
帕亨网络 <wz@paheng.com>
2024-10-30 19:09:02 +0800

    增加信息唯一性验证

--- app/admin/validate/AuthorValidate.php
+++ app/admin/validate/AuthorValidate.php
@@ -1,17 +1,36 @@
 <?php
 
 namespace app\admin\validate;
+
 use think\Validate;
+use think\facade\Db;
 
 class AuthorValidate extends Validate
 {
+
+    // 验证笔名
+    protected function checkOne($value, $rule, $data = [])
+    {
+        $count = Db::name('author')->where([['nickname', '=', $data['nickname']], ['id', '<>', $data['id']]])->count();
+        return $count == 0 ? true : false;
+    }
+
+    // 验证手机
+    protected function checkMobileOne($value, $rule, $data = [])
+    {
+        $count = Db::name('author')->where([['mobile', '=', $data['mobile']], ['id', '<>', $data['id']]])->count();
+        return $count == 0 ? true : false;
+    }
+
     protected $rule = [
-    'nickname' => 'require',
-    'mobile' => 'require',
-];
+        'nickname' => 'require|checkOne',
+        'mobile' => 'require|checkMobileOne',
+    ];
 
     protected $message = [
-    'nickname.require' => '昵称不能为空',
-    'mobile.require' => '手机不能为空',
-];
+        'nickname.require' => '笔名不能为空',
+        'nickname.checkOne' => '此笔名已经存在',
+        'mobile.require' => '手机不能为空',
+        'mobile.checkMobileOne' => '此手机已经存在',
+    ];
 }
