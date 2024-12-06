cec1cf41ad319307448b915509b116833bb19872
帕亨网络 <wz@paheng.com>
2024-12-06 20:07:50 +0800

    移动端适配评论功能

--- addons/changyan/config.php
+++ addons/changyan/config.php
@@ -5,49 +5,49 @@ return array (
   array (
     'title' => '畅言用户名',
     'type' => 'string',
-    'value' => '',
+    'value' => 'uudzs@qq.com',
     'tips' => '请填写畅言用户名',
   ),
   'password' => 
   array (
     'title' => '畅言密码',
     'type' => 'string',
-    'value' => '',
+    'value' => 'wc824945',
     'tips' => '请填写畅言密码',
   ),
   'siteid' => 
   array (
     'title' => '启用站点',
     'type' => 'string',
-    'value' => '',
+    'value' => 969902,
     'tips' => '请选择启用的站点',
   ),
   'pc_script' => 
   array (
     'title' => 'PC端安装代码',
     'type' => 'string',
-    'value' => '',
+    'value' => 'ICAgIDxzY3JpcHQgY2hhcnNldD0idXRmLTgiIHR5cGU9InRleHQvamF2YXNjcmlwdCIgc3JjPSJodHRwczovL2N5LWNkbi5rdWFpemhhbi5jb20vdXBsb2FkL2NoYW5neWFuLmpzIiA+PC9zY3JpcHQ+DQogICAgPHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiPg0KICAgIHdpbmRvdy5jaGFuZ3lhbi5hcGkuY29uZmlnKHsNCiAgICAgICAgYXBwaWQ6ICdjeXNoUjBTOGonLA0KICAgICAgICBjb25mOiAncHJvZF9kYzEwMjEzOWM2OGY3NDYwMTEzMDhhYmFhYmMxNzQzMycNCiAgICB9KTsNCiAgICA8L3NjcmlwdD4=',
     'tips' => '自动生成',
   ),
   'wap_script' => 
   array (
     'title' => 'WAP端安装代码',
     'type' => 'string',
-    'value' => '',
+    'value' => 'PHNjcmlwdCBpZD0iY2hhbmd5YW5fbW9iaWxlX2pzIiBjaGFyc2V0PSJ1dGYtOCIgdHlwZT0idGV4dC9qYXZhc2NyaXB0IiBzcmM9Imh0dHBzOi8vY3ktY2RuLmt1YWl6aGFuLmNvbS91cGxvYWQvbW9iaWxlL3dhcC1qcy9jaGFuZ3lhbl9tb2JpbGUuanM/Y2xpZW50X2lkPWN5c2hSMFM4aiZjb25mPXByb2RfZGMxMDIxMzljNjhmNzQ2MDExMzA4YWJhYWJjMTc0MzMiPjwvc2NyaXB0Pg==',
     'tips' => '自动生成',
   ),
   'adaptive_script' => 
   array (
     'title' => '自适应安装代码',
     'type' => 'string',
-    'value' => '',
+    'value' => 'PHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiPiANCihmdW5jdGlvbigpew0KICAgIHZhciBhcHBpZCA9ICdjeXNoUjBTOGonOyANCiAgICB2YXIgY29uZiA9ICdwcm9kX2RjMTAyMTM5YzY4Zjc0NjAxMTMwOGFiYWFiYzE3NDMzJzsgDQogICAgdmFyIHdpZHRoID0gd2luZG93LmlubmVyV2lkdGggfHwgZG9jdW1lbnQuZG9jdW1lbnRFbGVtZW50LmNsaWVudFdpZHRoOyANCiAgICBpZiAod2lkdGggPCAxMDAwKSB7DQogICAgICAgIHZhciBoZWFkID0gZG9jdW1lbnQuZ2V0RWxlbWVudHNCeVRhZ05hbWUoJ2hlYWQnKVswXXx8ZG9jdW1lbnQuaGVhZHx8ZG9jdW1lbnQuZG9jdW1lbnRFbGVtZW50Ow0KICAgICAgICB2YXIgc2NyaXB0ID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnc2NyaXB0Jyk7DQogICAgICAgIHNjcmlwdC50eXBlID0gJ3RleHQvamF2YXNjcmlwdCc7DQogICAgICAgIHNjcmlwdC5jaGFyc2V0ID0gJ3V0Zi04JzsNCiAgICAgICAgc2NyaXB0LmlkID0gJ2NoYW5neWFuX21vYmlsZV9qcyc7DQogICAgICAgIHNjcmlwdC5zcmMgPSAnaHR0cHM6Ly9jeS1jZG4ua3VhaXpoYW4uY29tL3VwbG9hZC9tb2JpbGUvd2FwLWpzL2NoYW5neWFuX21vYmlsZS5qcz9jbGllbnRfaWQ9JyArIGFwcGlkICsgJyZjb25mPScgKyBjb25mOw0KICAgICAgICBoZWFkLmFwcGVuZENoaWxkKHNjcmlwdCk7DQogICAgfSBlbHNlIHsgDQogICAgICAgIHZhciBsb2FkSnM9ZnVuY3Rpb24oZCxhKXsNCiAgICAgICAgICAgIHZhciBjPWRvY3VtZW50LmdldEVsZW1lbnRzQnlUYWdOYW1lKCJoZWFkIilbMF18fGRvY3VtZW50LmhlYWR8fGRvY3VtZW50LmRvY3VtZW50RWxlbWVudDsNCiAgICAgICAgICAgIHZhciBiPWRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoInNjcmlwdCIpOw0KICAgICAgICAgICAgYi5zZXRBdHRyaWJ1dGUoInR5cGUiLCJ0ZXh0L2phdmFzY3JpcHQiKTsNCiAgICAgICAgICAgIGIuc2V0QXR0cmlidXRlKCJjaGFyc2V0IiwiVVRGLTgiKTsNCiAgICAgICAgICAgIGIuc2V0QXR0cmlidXRlKCJzcmMiLGQpOw0KICAgICAgICAgICAgaWYodHlwZW9mIGE9PT0iZnVuY3Rpb24iKXsNCiAgICAgICAgICAgICAgICBpZih3aW5kb3cuYXR0YWNoRXZlbnQpew0KICAgICAgICAgICAgICAgICAgICBiLm9ucmVhZHlzdGF0ZWNoYW5nZT1mdW5jdGlvbigpew0KICAgICAgICAgICAgICAgICAgICAgICAgdmFyIGU9Yi5yZWFkeVN0YXRlOw0KICAgICAgICAgICAgICAgICAgICAgICAgaWYoZT09PSJsb2FkZWQifHxlPT09ImNvbXBsZXRlIil7DQogICAgICAgICAgICAgICAgICAgICAgICAgICAgYi5vbnJlYWR5c3RhdGVjaGFuZ2U9bnVsbDsNCiAgICAgICAgICAgICAgICAgICAgICAgICAgICBhKCkNCiAgICAgICAgICAgICAgICAgICAgICAgIH0NCiAgICAgICAgICAgICAgICAgICAgfQ0KICAgICAgICAgICAgICAgIH1lbHNlew0KICAgICAgICAgICAgICAgICAgICBiLm9ubG9hZD1hDQogICAgICAgICAgICAgICAgfQ0KICAgICAgICAgICAgfQ0KICAgICAgICAgICAgYy5hcHBlbmRDaGlsZChiKQ0KICAgICAgICB9Ow0KICAgICAgICBsb2FkSnMoImh0dHBzOi8vY3ktY2RuLmt1YWl6aGFuLmNvbS91cGxvYWQvY2hhbmd5YW4uanMiLGZ1bmN0aW9uKCl7DQogICAgICAgICAgICB3aW5kb3cuY2hhbmd5YW4uYXBpLmNvbmZpZyh7YXBwaWQ6YXBwaWQsY29uZjpjb25mfSkNCiAgICAgICAgfSk7DQogICAgfQ0KfSkoKTsNCjwvc2NyaXB0Pg==',
     'tips' => '自动生成',
   ),
   'pc_scoring_script' => 
   array (
     'title' => 'PC打分版安装代码',
     'type' => 'string',
-    'value' => '',
+    'value' => 'PHNjcmlwdCBjaGFyc2V0PSJ1dGYtOCIgdHlwZT0idGV4dC9qYXZhc2NyaXB0IiBzcmM9Imh0dHBzOi8vY3ktY2RuLmt1YWl6aGFuLmNvbS91cGxvYWQvY2hhbmd5YW4uanMiID48L3NjcmlwdD4NCjxzY3JpcHQgdHlwZT0idGV4dC9qYXZhc2NyaXB0Ij4NCndpbmRvdy5fY29uZmlnID0geyBzaG93U2NvcmU6IHRydWUgfTsNCndpbmRvdy5jaGFuZ3lhbi5hcGkuY29uZmlnKHsNCiAgICBhcHBpZDogJ2N5c2hSMFM4aicsDQogICAgY29uZjogJ3Byb2RfZGMxMDIxMzljNjhmNzQ2MDExMzA4YWJhYWJjMTc0MzMnDQp9KTsNCjwvc2NyaXB0Pg==',
     'tips' => '自动生成',
   ),
 );
