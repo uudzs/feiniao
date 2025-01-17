c7270cc1ec00553b8a851212bc9408c9ab701c32
帕亨网络 <wz@paheng.com>
2025-01-17 19:09:28 +0800

    更新路由规则

--- route/app.php
+++ route/app.php
@@ -3,4 +3,7 @@
 use think\facade\Route;
 Route::rule('cate-<id>', 'book/cate', 'GET|POST')->name('book_cates');
 Route::rule('book-:id', 'book/detail', 'GET|POST')->name('book_detail');
+Route::rule('login$', 'login/index', 'GET|POST')->name('login');
+Route::rule('author-:id', 'author/detail', 'GET|POST')->name('author_detail');
+Route::rule('chapter-:id', 'chapter/detail', 'GET|POST')->name('chapter_detail');
 Route::miss('\app\home\controller\Emptys::miss');
