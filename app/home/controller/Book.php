bb20d1c6755a711bbaf0f63285cff9a5c9dd951e
帕亨网络 <wz@paheng.com>
2025-02-27 11:36:41 +0800

    作品页增加一些属性标签提升SEO作用

--- app/home/controller/Book.php
+++ app/home/controller/Book.php
@@ -8,6 +8,7 @@ use app\home\BaseController;
 use think\facade\Db;
 use think\facade\View;
 use think\facade\Request;
+use think\facade\Route;
 
 class Book extends BaseController
 {
@@ -27,9 +28,9 @@ class Book extends BaseController
         } else {
             $category = Db::name('category')->where(['key' => $id])->find();
             if (!empty($category)) {
-                $id = $category['id'];           
+                $id = $category['id'];
             }
-        }        
+        }
         View::assign('category', $category);
         View::assign('catid', $id);
         View::assign('id', $id);
@@ -76,15 +77,46 @@ class Book extends BaseController
         $ismakecache = $this->usecache();
         $param = get_params();
         $id = isset($param['id']) ? $param['id'] : 0;
+        $book = [];
         if (intval($id) ==  $id) {
-            Db::name('book')->where('id', $id)->inc('hits')->update();
+            $book = Db::name('book')->where('id', $id)->find();
+            if (!empty($book)) {
+                Db::name('book')->where('id', $id)->inc('hits')->update();
+            }
         } else {
             $book = Db::name('book')->where('filename', $id)->find();
             if (!empty($book)) {
                 $id = $book['id'];
-                Db::name('book')->where('id', $id)->inc('hits')->update();                
+                Db::name('book')->where('id', $id)->inc('hits')->update();
             }
         }
+        if (!empty($book)) {
+            $book['bigclassname'] = Db::name('category')->where(['id' => $book['genre']])->value('name');
+            $book['cover'] = get_file($book['cover']);
+            $book['words'] = wordCount($book['words']);
+            $book['author'] = trim($book['author']);
+            $book['remark'] = $book['remark'] ? strip_tags($book['remark']) : '暂无简介';
+            $first_chapter = Db::name('chapter')->field('id,title')->where(['bookid' => $id, 'status' => 1, ['verify', 'in', '0,1']])->order('chaps asc')->find();
+            if (!empty($first_chapter)) {
+                $book['first_chapter_url'] = (string) Route::buildUrl('chapter_detail', ['id' => $first_chapter['id']])->domain(true);
+                $book['first_chapter_title'] = $first_chapter['title'];
+            } else {
+                $book['first_chapter_url'] = '';
+                $book['first_chapter_title'] = '';
+            }
+            $chapter = Db::name('chapter')->field('id,title,create_time,update_time')->where(['bookid' => $id, 'status' => 1, ['verify', 'in', '0,1']])->order('chaps desc')->find();
+            if (!empty($chapter)) {
+                $book['chapter_url'] = (string) Route::buildUrl('chapter_detail', ['id' => $chapter['id']])->domain(true);
+                $book['chapter_title'] = $chapter['title'];
+                $book['update_time'] =  $chapter['update_time'] ? date('Y-m-d H:i:s', $chapter['update_time']) : date('Y-m-d H:i:s', $chapter['create_time']);
+            } else {
+                $book['chapter_url'] = '';
+                $book['chapter_title'] = '';
+                $book['update_time'] = '';
+            }
+            $book['authorurl'] = (string) Route::buildUrl('author_detail', ['id' => $book['authorid']])->domain(true);
+        }
+        View::assign('book', $book);
         View::assign('bid', $id);
         if ($ismakecache) $this->makecache(View::fetch());
         return view();
