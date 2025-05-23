76160c375748b30b8cc9e8941bb6dfea6feb9ff5
帕亨网络 <wz@paheng.com>
2025-05-23 15:42:30 +0800

    重写错误捕获机制

--- app/provider.php
+++ app/provider.php
@@ -1,8 +1,4 @@
 <?php
-
-// 容器Provider定义文件
 return [
-    // 绑定自定义异常处理handle类
-    'think\exception\Handle'       => '\\app\\exception\\Http',
-    'think\exception\Handle'       => '\\app\\exception\\Handler',
+    'think\exception\Handle' => '\app\exception\Handler',
 ];
